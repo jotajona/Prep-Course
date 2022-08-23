0dbc2d34f3f97aabb1db00920c59026820f17ce2
jotajona <jotajona1@gmail.com>
2022-08-23 14:55:24 -0700

    test 082322

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -129,10 +129,10 @@ if ( numero % 5 === 0 && numero % 3 === 0){
   return "buzz";
 }else if (numero % 3 === 0){
   return "fizz";
-}else{
-  return numero;
 }
+return numero;
 }
+
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
@@ -141,6 +141,21 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 === 0 || num2 === 0 || num3 === 0){
+    return "Error"
+  }
+  if (num1 < 0 || num2 < 0 || num3 < 0){
+    return "Hay negativos";
+  }
+  if ( num1 > num2 && num1 > num3 && num1 > 0){
+  return "Número 1 es mayor y positivo";
+}
+if (num3 > num2 && num3 > num1){
+  return num3 + 1;
+}
+else{
+return false
+}
 }
 
 function esPrimo(numero) {
