fc01dea9c19516f51acf2c0327e85d7268840173
jotajona <jotajona1@gmail.com>
2022-08-22 22:44:42 -0700

    test 082222

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -32,6 +32,14 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+if ( status!== 1 || status !== 2){
+  return "Offline"
+}
+if (status == 1){
+  return "Online"
+}else{
+  return "Away"
+}
 }
 
 function saludo(idioma) {
